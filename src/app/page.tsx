1f9b86d923df05a6b5da0b460346669f35b97c2d
Akash Kumar <akashsingh4152@gmail.com>
2026-02-11 15:17:56 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,21 @@ import MangaAbout from "@/components/MangaStyle/MangaAbout";
 import FeaturedProjects from "@/components/MangaStyle/MangaFeatures";
 import MangaHero from "@/components/MangaStyle/MangaHero";
 import TechSection from "@/components/MangaStyle/MangaTech";
-import ProjectCard from "@/components/ProjectCard";
-import TechMarquee from "@/components/TechMarquee";
-import { motion } from "framer-motion";
-
 
 export default function Home() {
-  
-
   return (
     <main className="min-h-screen ">
       {/* HERO */}
       <MangaHero />
-      
+
       {/* ABOUT */}
       <MangaAbout />
 
       {/* PROJECTS */}
-    <FeaturedProjects />
+      <FeaturedProjects />
 
       {/* TECH */}
-<TechSection />
+      <TechSection />
     </main>
   );
 }
